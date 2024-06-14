1f1b447a01fb1513e7cfbd2aafcde01d71b156fa
liuxiaoyu47 <liuxiaoyu47@meituan.com>
2024-06-14 13:35:44 +0800

    update run.sh and change struct of this project

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # 清楚原有的源文档并上传新的
 rm -rf content/posts/copyMD
-mkdir content/posts/copyMD
-cp - ../content/copyMD/* ./content/posts/copyMD
+mkdir -p content/posts/copyMD
+cp -rf ../content/copyMD/* ./content/posts/copyMD
 # 清楚原有静态文件
-rm -rf public/*
+rm -rf ./public
 # 生成静态页面
 mkdir public
 hugo
